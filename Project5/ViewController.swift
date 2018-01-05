fd11d85375de12c4fd10c3fc8c8be92de5849d0d
Sf1770 <sf1770@nyu.edu>
2018-01-05 17:20:09 -0500

    Word_Scramble_Final

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -5,20 +5,150 @@
 //  Created by Sabrina Fletcher on 12/20/17.
 //  Copyright © 2017 Sabrina Fletcher. All rights reserved.
 //
+/* Need to check if the word has been used already
+ Need to check whether the word can be made from the given letters
+ Need to check if the word is a valid English word
+ */
+/*If using UIKit, SpriteKit, and Apple framework use utf16.count for character count; otherwise loop over characters and process each on individually using count */
 
 import UIKit
+import GameplayKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt"){
+            
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loadDefaultWords() {
+        allWords = ["silkworm"]
     }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] _ in
+            let answer = ac.textFields![0]
+            
+            self.submit(answer: answer.text!)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    
+    func submit(answer: String){
+        let lowerAnswer = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
+        
+        if isPossible(word: lowerAnswer){
+            if isOriginal(word: lowerAnswer){
+                if isReal(word: lowerAnswer){
+                    usedWords.insert(answer, at: 0)
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    return
+                    
+                } else {
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
+                }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
+                showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
+            }
+        } else {
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title!.lowercased())!"
+            showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
+        }
+        
+        
+    }
+    
+    func showErrorMessage(errorTitle: String, errorMessage: String) {
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = title!.lowercased()
+        
+        for letter in word{
+            if let pos = tempWord.range(of: String(letter)){
+                tempWord.remove(at: pos.lowerBound)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        if (!usedWords.contains(word)) && word == allWords[0]{
+            return false
+        } else{
+            return true
+        }
+    }
+    
+    func isReal(word: String) -> Bool{
+        let checker = UITextChecker()
+        let range = NSMakeRange(0, word.utf16.count)
+        
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        if (misspelledRange.location == NSNotFound) && word.count < 3{
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        
+        cell.textLabel?.text = usedWords[indexPath.row]
+        
+        return cell
+    }
+    
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
 
 
 }
